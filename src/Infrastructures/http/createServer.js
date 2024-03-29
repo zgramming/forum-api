4fe9804154566f699d15865bbbf098815130c5d9
Zeffry Reynando <zeffry.reynando@gmail.com>
2024-03-29 15:10:51 +0700

    Feat: Like Comment HTTP

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -59,10 +59,10 @@ const createServer = async (container) => {
       plugin: replies,
       options: { container },
     },
-    // {
-    //   plugin: likes,
-    //   options: { container },
-    // },
+    {
+      plugin: likes,
+      options: { container },
+    },
   ]);
 
   server.ext('onPreResponse', (request, h) => {
